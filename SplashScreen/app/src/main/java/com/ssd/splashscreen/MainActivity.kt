7fb74d488b82fbe53139e69198a6c19a907fa235
asdf <vot768@naver.com>
2022-09-05 16:31:18 +0900

    1. splash screen

---
+++ SplashScreen/app/src/main/java/com/ssd/splashscreen/MainActivity.kt
@@ -0,0 +1,17 @@
+package com.ssd.splashscreen
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "MainActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+
+}
---
+++ SplashScreen/app/src/main/java/com/ssd/splashscreen/SplashActivity.kt
@@ -0,0 +1,31 @@
+package com.ssd.splashscreen
+
+import android.content.Intent
+import android.os.Bundle
+import android.os.Handler
+import android.os.Looper
+import androidx.appcompat.app.AppCompatActivity
+
+class SplashActivity : AppCompatActivity() {
+
+    companion object {
+        const val TAG: String = "SplashActivity"
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_splash)
+
+        Handler(Looper.getMainLooper()).postDelayed({
+            val intent = Intent(this, MainActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            finish()
+        }, 3000)
+    }
+
+    override fun onBackPressed() {
+        super.onBackPressed()
+    }
+
+}
